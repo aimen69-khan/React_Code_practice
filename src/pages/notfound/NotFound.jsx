53d10ca923c27f2e47174b9aa724635d7b00ebcd
Ahmed Raza <aimen69khan@gmail.com>
2026-08-08 10:21:41 +0500

    Not Found page done

---
+++ src/pages/notfound/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from '../../components/Nav/Nav'
+
+
+function NotFound() {
+  return (
+    <>
+      <Nav />
+      <h1>404 --- error Page Not Found!!</h1>
+    </>
+  )
+}
+
+
+export default NotFound
