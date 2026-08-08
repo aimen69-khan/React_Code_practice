2451f35d9cbeaa08ee1fb1b2dfa125c724584da2
Ahmed Raza <aimen69khan@gmail.com>
2026-08-08 10:16:12 +0500

    Footer complete

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <p>THIS IS FOOTER!!</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
