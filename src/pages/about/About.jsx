764bfd5347243c1cfcdbb5264318b369ff0d77b4
Ahmed Raza <aimen69khan@gmail.com>
2026-08-08 10:20:35 +0500

    about page done

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../../components/Nav/Nav'
+import Footer from '../../components/Footer/Footer'
+
+function About() {
+  return (
+    <>
+      <Nav />
+      <Footer />
+    </>
+  )
+}
+
+export default About
