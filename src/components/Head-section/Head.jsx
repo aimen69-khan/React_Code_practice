27b64c2bb401fc9230dbec4b61b748281642a3e2
Ahmed Raza <aimen69khan@gmail.com>
2026-08-08 10:14:40 +0500

    Head_section - completed

---
+++ src/components/Head-section/Head.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <>
+    <div className='Head-section'>
+        <img src='' alt='HERO SECION' />
+    </div>
+    </>
+  )
+}
+
+export default Head
