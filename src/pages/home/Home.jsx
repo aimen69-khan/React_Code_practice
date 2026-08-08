3cc7ce6e1f2509801e8607aa5a2425e2e6c1eeed
Ahmed Raza <aimen69khan@gmail.com>
2026-08-08 10:19:12 +0500

    Home page done

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import Head from "../../components/Head-section/Head";
+import Nav from "../../components/Nav/Nav";
+
+function Home(){
+    <>
+    <Nav />
+    <Head />
+    </>
+}
+
+export default Home;
